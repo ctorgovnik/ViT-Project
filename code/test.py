60a4b1f56419c9d7f666e44557e04fa830a7e5e2
cjt76 <cjt76@cornell.edu>
2025-04-24 13:05:47 -0400

    Restructure repo to specified format

---
+++ code/test.py
@@ -0,0 +1,21 @@
+import torch
+def test(model, test_loader, criterion, device):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
+    
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(test_loader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+            test_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+            
+    test_loss = test_loss / len(test_loader)
+    test_acc = 100. * correct / total
+    return test_loss, test_acc
+            
